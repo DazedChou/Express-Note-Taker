265ae456bd5940bee9a820cbebcbb2a53609cd63
David Chou <davidchou2009@gmail.com>
2022-01-30 16:23:20 -0800

    create instance of express and implement middleware

--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const noteData = require('./db/db.json');
 const path = require('path');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
